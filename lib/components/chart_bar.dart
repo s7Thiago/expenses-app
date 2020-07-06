efceb12da952b32578bee9c40dc6a9fb4a684bf5
Thiago Silva <37230912+s7Thiago@users.noreply.github.com>
2020-07-06 10:41:16 -0300

    Making the chart height adaptive

--- lib/components/chart_bar.dart
+++ lib/components/chart_bar.dart
@@ -13,45 +13,53 @@ class ChartBar extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Container(
-          height: 20,
-          child: FittedBox(
-            child: Text('${value.toStringAsFixed(2)}'),
+    return LayoutBuilder(
+        builder: (BuildContext context, BoxConstraints constraints) {
+      return Column(
+        children: [
+          Container(
+            height: constraints.maxHeight * .15,
+            child: FittedBox(
+              child: Text('${value.toStringAsFixed(2)}'),
+            ),
           ),
-        ),
-        Container(
-          margin: EdgeInsets.symmetric(vertical: 5),
-          height: 60,
-          width: 10,
-          child: Stack(
-            alignment: Alignment.bottomCenter,
-            children: [
-              Container(
-                decoration: BoxDecoration(
-                  border: Border.all(
-                    color: Colors.grey,
-                    width: 1,
-                  ),
-                  color: Color.fromRGBO(220, 220, 220, 1),
-                  borderRadius: BorderRadius.circular(5),
-                ),
-              ),
-              FractionallySizedBox(
-                heightFactor: percentage,
-                child: Container(
+          Container(
+            margin: EdgeInsets.symmetric(vertical: constraints.maxHeight * .05),
+            height: constraints.maxHeight * .6,
+            width: 10,
+            child: Stack(
+              alignment: Alignment.bottomCenter,
+              children: [
+                Container(
                   decoration: BoxDecoration(
-                    color: Theme.of(context).primaryColor,
+                    border: Border.all(
+                      color: Colors.grey,
+                      width: 1,
+                    ),
+                    color: Color.fromRGBO(220, 220, 220, 1),
                     borderRadius: BorderRadius.circular(5),
                   ),
                 ),
-              )
-            ],
+                FractionallySizedBox(
+                  heightFactor: percentage,
+                  child: Container(
+                    decoration: BoxDecoration(
+                      color: Theme.of(context).primaryColor,
+                      borderRadius: BorderRadius.circular(5),
+                    ),
+                  ),
+                )
+              ],
+            ),
           ),
-        ),
-        Text(label),
-      ],
-    );
+          Container(
+            height: constraints.maxHeight * .15,
+            child: FittedBox(
+              child: Text(label),
+            ),
+          ),
+        ],
+      );
+    });
   }
 }
